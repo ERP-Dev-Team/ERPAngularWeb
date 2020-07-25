ddacafb82b9546cd6a49f5bf79ad9de8f5177782
humanSeeMe <ganesh96ast@gmail.com>
2020-07-25 23:53:18 +0530

    ITEM CATEGORY COMMITT

--- src/app/views/itemCategories/view-item-category/view-item-category.component.ts
+++ src/app/views/itemCategories/view-item-category/view-item-category.component.ts
@@ -1,15 +1,54 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
+
+const ITEMCATEGORIES_QUERY = gql`
+  query {
+    itemcategories {
+      _id
+      name
+    }
+  }
+`;
 
 @Component({
   selector: 'app-view-item-category',
   templateUrl: './view-item-category.component.html',
-  styleUrls: ['./view-item-category.component.css']
+  styleUrls: ['./view-item-category.component.css'],
 })
 export class ViewItemCategoryComponent implements OnInit {
+   public itemCategoryList: any;
+   private responseGetter: any;
 
-  constructor() { }
+  constructor(
+    private apollo: Apollo,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { this.getAvailableItemCategories()}
 
-  ngOnInit(): void {
+  private getAvailableItemCategories(){
+
+    this.apollo.query({
+   query: ITEMCATEGORIES_QUERY
+    }).subscribe( (result) => {
+        this.responseGetter = result.data;
+        this.itemCategoryList = this.responseGetter.itemcategories
+    },   (error) => {
+      console.log('there was an error sending the query', error);
+    });
   }
 
+  public onItemCategoryEdit(itemCategory:any){
+    this.router.navigate(['/editItemCategory'], {queryParams: {id: itemCategory._id}, skipLocationChange: true})
+  }
+
+  public navigativeToAddItemCategory(){
+    this.router.navigate(['/addItemCategory'])
+  }
+
+
+  ngOnInit(): void {
+    this.getAvailableItemCategories()
+  }
 }
