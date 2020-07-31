033ac25a05125b83e5d5958beabe7810204d9aea
Vamsi <kothapallivamsikrishna@gmail.com>
2020-07-31 01:26:22 -0500

    setting up environment for staging and production

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true
+  production: true,
+  apiEndPoint:"http://chicago.opcl.net:3000/api"
 };
 
 /*
